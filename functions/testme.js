d48d91b826f3fa7ad4818153d8b5a14e3875defb
barbieessm <barbieessm@proton.me>
2022-09-12 16:49:55 +0700

    re-submit

--- functions/testme.js
+++ functions/testme.js
@@ -2,9 +2,9 @@ export default (req, res) => {
 
     console.log('<< req.headers >>', req.headers);
     console.dir(req, { depth: 2 });
-    console.log('<< req.apiGateway >>', req.apiGateway);
-    console.log('<< req.requestContext >>', req.requestContext);
-    console.log('<< req.client.address() >>', req.client.address());
+    // console.log('<< req.apiGateway >>', req.apiGateway);
+    // console.log('<< req.requestContext >>', req.requestContext);
+    // console.log('<< req.client.address() >>', req.client.address());
 
 
     const timestamp = Date.now() / 1000 | 0;
