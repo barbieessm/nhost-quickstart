06003f606aa036754f79b593f8ea5c957745601a
barbieessm <barbieessm@proton.me>
2022-09-12 16:35:57 +0700

    testme ip address

--- functions/testme.js
+++ functions/testme.js
@@ -1,8 +1,11 @@
 export default (req, res) => {
-    // console.dir(req, { depth: 1 });
+
+    console.log('<< req.headers >>', req.headers);
+    console.dir(req, { depth: 2 });
     console.log('<< req.apiGateway >>', req.apiGateway);
     console.log('<< req.requestContext >>', req.requestContext);
     console.log('<< req.client.address() >>', req.client.address());
+
 
     const timestamp = Date.now() / 1000 | 0;
 
